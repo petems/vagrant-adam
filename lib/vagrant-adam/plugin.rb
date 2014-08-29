b23f7314722eaf02f910959ec2f4902cd846e21c
Peter Souter <p.souter@kainos.com>
2014-08-29 14:09:38 +0100

    Awesome way of checking Vagrant version!
    Totally stolen from @tmatilai in his excellent https://github.com/tmatilai/vagrant-proxyconf code :+1:

--- lib/vagrant-adam/plugin.rb
+++ lib/vagrant-adam/plugin.rb
@@ -1,14 +1,36 @@
-if Vagrant::VERSION < '1.1.0'
-  fail 'The Vagrant Adam plugin is only compatible with Vagrant 1.1+'
-end
-
 module VagrantPlugins
   module Adam
     class Plugin < Vagrant.plugin('2')
+
+      VAGRANT_VERSION_REQUIREMENT = '>= 1.1.0'
+
       name 'vagrant-adam'
       description <<-DESC
       This plugin ensures a pre-provision script is run before anything else
       DESC
+
+      # Returns true if the Vagrant version fulfills the requirements
+      #
+      # @param requirements [String, Array<String>] the version requirement
+      # @return [Boolean]
+      def self.check_vagrant_version(*requirements)
+        Gem::Requirement.new(*requirements).satisfied_by?(
+          Gem::Version.new(Vagrant::VERSION))
+      end
+
+      # Verifies that the Vagrant version fulfills the requirements
+      #
+      # @raise [VagrantPlugins::ProxyConf::VagrantVersionError] if this plugin
+      # is incompatible with the Vagrant version
+      def self.check_vagrant_version!
+        if !check_vagrant_version(VAGRANT_VERSION_REQUIREMENT)
+          msg = I18n.t(
+            'vagrant_adam.errors.vagrant_version',
+            requirement: VAGRANT_VERSION_REQUIREMENT.inspect)
+          $stderr.puts msg
+          raise msg
+        end
+      end
 
       action_hook(:pre_provision_script, Plugin::ALL_ACTIONS) do |hook|
         require_relative 'action/pre_provision_script'
