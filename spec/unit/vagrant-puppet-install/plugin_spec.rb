ad8ca686edf3d12c7f2fbe2a9c50dc3b911c0139
Peter Souter <p.souter@kainos.com>
2014-09-01 23:36:51 +0100

    Adding plugin_spec

---
+++ spec/unit/vagrant-puppet-install/plugin_spec.rb
@@ -0,0 +1,80 @@
+require_relative '../spec_helper'
+
+describe VagrantPlugins::Adam::Plugin do
+
+  context 'action hooks' do
+    let(:hook) {double(append: true, prepend: true)}
+
+    it 'should hook PreProvisionScript before SyncedFolders' do
+      hook_proc = described_class.components.action_hooks[:__all_actions__][0]
+      hook = double
+      expect(hook).to receive(:after).with(Vagrant::Action::Builtin::SyncedFolders, VagrantPlugins::Adam::Action::PreProvisionScript)
+      hook_proc.call(hook)
+    end
+  end
+
+  it "should define a config of type :adam" do
+    default_config = described_class.components.configs[:top].to_hash[:"adam"]
+    expect(default_config).to be(VagrantPlugins::Adam::Config)
+  end
+
+  describe '.check_vagrant_version' do
+    before :each do
+      stub_const('Vagrant::VERSION', '1.2.3')
+    end
+
+    it "accepts single String argument" do
+      expect(described_class.check_vagrant_version('~> 1.1')).to be_true
+      expect(described_class.check_vagrant_version('1.2')).to be_false
+    end
+
+    it "accepts an Array argument" do
+      expect(described_class.check_vagrant_version(['>= 1.1', '< 1.3.0.beta'])).to be_true
+      expect(described_class.check_vagrant_version(['>= 1.3'])).to be_false
+    end
+
+    it "accepts multiple arguments" do
+      expect(described_class.check_vagrant_version('>= 1.0', '<= 1.3')).to be_true
+      expect(described_class.check_vagrant_version('~> 1.2', '>= 1.2.5')).to be_false
+    end
+  end
+
+  describe '.check_vagrant_version!' do
+    subject { described_class.check_vagrant_version! }
+    let(:requirement) { '>= 1.1.0' }
+    let(:err_msg) { /requires Vagrant version #{Regexp.escape(requirement.inspect)}/ }
+
+    before :each do
+      stub_const(
+        'VagrantPlugins::ProxyConf::Plugin::VAGRANT_VERSION_REQUIREMENT',
+        requirement)
+      stub_const('Vagrant::VERSION', vagrant_version)
+      $stderr.stub(:puts)
+    end
+
+    context "on too old Vagrant version" do
+      let(:vagrant_version) { '1.0.9' }
+      it "raises error" do
+        expect { subject }.to raise_error(err_msg)
+      end
+      it "warns as stderr" do
+        $stderr.should_receive(:puts).with(err_msg)
+        subject rescue nil
+      end
+    end
+
+    context "on exact required Vagrant version" do
+      let(:vagrant_version) { '1.1.0' }
+      it "does not raise" do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context "on newer Vagrant version" do
+      let(:vagrant_version) { '1.3.5' }
+      it "does not raise" do
+        expect { subject }.not_to raise_error
+      end
+    end
+  end
+end
