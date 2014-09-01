0cc17f9ebeff4c757bad9307d237676f57d7ee2a
Peter Souter <p.souter@kainos.com>
2014-09-01 23:35:16 +0100

    Adding validation for provision_url

--- lib/vagrant-adam/config.rb
+++ lib/vagrant-adam/config.rb
@@ -17,6 +17,38 @@ module VagrantPlugins
       def validate!(machine)
         finalize!
         errors = []
+
+        if (@provision_url != nil)
+          unless (valid_uri? @provision_url) || (valid_file? @provision_url)
+            msg = <<-EOH
+'#{ @provision_url }' is not a valid filepath or URL
+            EOH
+            errors << msg
+          end
+        end
+
+        if errors.any?
+          rendered_errors = Vagrant::Util::TemplateRenderer.render(
+                              'config/validation_failed',
+                              errors: { 'vagrant-adam' => errors }
+                            )
+          fail Vagrant::Errors::ConfigInvalid, errors: rendered_errors
+        end
+      end
+
+      private
+
+      def valid_uri? value
+        begin
+          uri = URI.parse value
+          uri.kind_of?(URI::HTTP) or uri.kind_of?(URI::HTTPS)
+        rescue URI::InvalidURIError
+          false
+        end
+      end
+
+      def valid_file? value
+        File.file?(value)
       end
 
     end
