0f11ff2d3076fa3ed3793a66450147ccec0289e0
Peter Souter <p.souter@kainos.com>
2014-10-02 16:36:52 +0100

    Fixed specs for when adam config absent
    Basic unit test to say it shouldn't run the provision script if the config value hasn't been set.

--- spec/unit/vagrant-puppet-install/action/pre_provision_script_spec.rb
+++ spec/unit/vagrant-puppet-install/action/pre_provision_script_spec.rb
@@ -13,7 +13,10 @@ describe VagrantPlugins::Adam::Action::PreProvisionScript do
     double('machine').tap { |machine| machine.stub(config: config, communicate: communicate) }
   end
   let(:communicate) do
-    double('communicate').tap { |machine| machine.stub(upload: upload) }
+    double('communicate').tap { |communicate| communicate.stub(upload: upload, ready?: ready?) }
+  end
+  let(:ready?) do
+    double('ready?')
   end
   let(:upload) do
     double('upload')
@@ -25,7 +28,7 @@ describe VagrantPlugins::Adam::Action::PreProvisionScript do
     double('vm').tap { |config| config.stub(guest: 'guest') }
   end
   let(:adam) do
-    double('adam').tap { |config| config.stub(:finalize! => true, :provision_url => '/tmp/config.sh') }
+    double('adam').tap { |config| config.stub(:finalize! => true, :provision_url => '/tmp/config.sh', :validate! => true,) }
   end
 
   describe '#initialize' do
@@ -34,11 +37,23 @@ describe VagrantPlugins::Adam::Action::PreProvisionScript do
   end
 
   describe '#call' do
-    context 'when called' do
+    context 'when called with config set' do
       subject(:pre_provision_script) { described_class.new(app, env) }
       it 'should fetch the script, then run it' do
         pre_provision_script.should_receive(:fetch_or_create_pre_provision_script).with(env)
         pre_provision_script.should_receive(:run_provision_script).with(env)
+        pre_provision_script.call(env)
+      end
+    end
+
+    context 'when called without config set' do
+      subject(:pre_provision_script) { described_class.new(app, env) }
+      let(:adam) do
+        double('adam').tap { |config| config.stub(:finalize! => true, :provision_url => nil, :validate! => true) }
+      end
+      it 'exit early' do
+        pre_provision_script.should_not_receive(:fetch_or_create_pre_provision_script).with(env)
+        pre_provision_script.should_not_receive(:run_provision_script).with(env)
         pre_provision_script.call(env)
       end
     end
